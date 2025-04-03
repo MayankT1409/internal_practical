a545b65386e6391498b567d06f2d91d7ee05518c
MayankT1409 <23it135@charusat.edu.in>
2025-04-03 11:00:06 +0530

    update

--- Client/src/Component/Footer.jsx
+++ Client/src/Component/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
     <div  className='footer mt-5' style={{ boxShadow:'-4px -4px 14px rgb(13,202,240)'}}>
     <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center fs-5"  style={{color:'rgb(13,202,240)'}}>Copyright &copy; My ❤️ Website 2024</p></div>
+            <div class="container"><p class="m-0 text-center fs-5"  style={{color:'rgb(13,202,240)'}}>Copyright &copy; PhotoGallery</p></div>
           </footer>
     </div>
     </>
